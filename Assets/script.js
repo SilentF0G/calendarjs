99a77d5a3ea6f456524726dd0be1409a22ea527f
SilentF0G <kornakov.y@mail.ru>
2024-05-01 09:25:14 +0300

    test4

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,12 @@
-alert("Скрипт подключен");
-document.getElementById('data-today').innerHTML = new Date();
+function createCalendar(elem, year, month) {
+	elem = document.querySelector(elem);
+
+	let mon = month - 1;
+	let d = new Date(year, month);
+}
+
+function getDay(date) {
+	let day = date.getDay();
+	if (day == 0) day = 7;
+	return day - 1;
+}
