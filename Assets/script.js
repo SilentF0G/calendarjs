44142052417c5ca37d1d9227331042b32273feff
SilentF0G <kornakov.y@mail.ru>
2024-05-02 15:38:27 +0300

    ver0.0.3

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,32 @@
 document.getElementById('theDateTwo').valueAsDate = new Date();
-alert("succes");
-document.getElementById('button-succes').onclick = function getDate(){
-    let dateDip = new Date();
-    let dateStart = document.getElementById('theDateOne').value;
-    let dateEnd = document.getElementById('theDateTwo').value;
-    dateStart = Date.parse(dateStart);
-    dateEnd = Date.parse(dateEnd);
+
+alert("JS подключён");
+
+document.getElementById('buttonSucces').onclick = function getDate(){
+    let dipDate = new Date();
+    let dateStart = document.getElementById('theDateOne').valueAsDate;
+    let dateEnd = document.getElementById('theDateTwo').valueAsDate;
     let out = document.getElementById('out');
-    for (i = dateStart; i < dateEnd; i = i+24*60*60*1000){
-       dateDip += new Date(i).toISOString().substr(0, 10);
+
+    let dateDipDay = dateEnd - dateStart;
+    let dateConvert = 1000 * 60 * 60 * 24;
+    dateDipDay = dateDipDay / dateConvert;
+
+    if(dateEnd > dipDate){
+        document.getElementById('checkDate').innerHTML = "Выберите дату не позднее сегодняшнего дня";
+    }
+    else{
+        let yearsDate = Math.floor(dateDipDay/365);
+        let months = Math.floor(dateDipDay/30);
+        document.getElementById('daysDate').innerHTML = "Дней " + dateDipDay;
+        document.getElementById('yearsDate').innerHTML = "Лет " + yearsDate;
+        document.getElementById('monthsDate').innerHTML = "Месяцев " + months;
+        document.getElementById('checkDate').innerHTML = "Проверка даты не позднее сегодня";
     }
 }
+var months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 
-document.getElementById('button-2').onclick = function showDate(){
-    document.getElementById('theDateOne').show;
-    document.getElementById('theDateTwo').show;
+function convertDate(date_str) {
+  temp_date = date_str.split("-");
+  return temp_date[2] + " " + months[Number(temp_date[1]) - 1] + " " + temp_date[0];
 }
